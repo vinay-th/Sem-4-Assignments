1d479efc94ec1827ce512485c6d861e8b73ee585
Vinay Thakor <vinay.th.24@gmail.com>
2024-12-23 18:27:21 +0530

    Add dialog boxes to InputJOptionPane for user interaction and feedback

--- Learn-Concepts/Core-Java/InputJOptionPane.java
+++ Learn-Concepts/Core-Java/InputJOptionPane.java
@@ -13,5 +13,32 @@ public class InputJOptionPane {
 
         System.out.println("The number you entered is : " + res);
 
+        // This is a normal message dialog box that displays the message
+        JOptionPane.showMessageDialog(null, "Toh kaise hai aap log", "Hello", JOptionPane.PLAIN_MESSAGE);
+
+        // this is a confirm dialog box
+        int x = JOptionPane.showConfirmDialog(null, "Kya aap zinda hai??", "Life update bro! 🙂",    JOptionPane.ERROR_MESSAGE);
+
+        // dekh bro yaha pr na
+        // 1st Arg is null parent component (kaha pr khulega dialog)
+        // 2nd Arg is our message
+        // 3rd Arg is our title of dialog
+        // 4th Arg is our No default options
+        // 5th Arg is our Logo
+        // 6th Arg is our null
+        // 7th Arg is array of our options
+        // 8th Arg is out initial value it goes like array index
+        String[] options = { "naa", "mood nai hota", "subjects tak nai pata" };
+
+        int y = JOptionPane.showOptionDialog(null, "Kya aap padhte hai??", "Life update bro! 🙂", JOptionPane.NO_OPTION,
+                JOptionPane.INFORMATION_MESSAGE, null, options, null);
+
+        if (y == 0) {
+            System.out.println("Saras");
+        } else if (y == 1) {
+            System.out.println("Bhanvanu kar dofa");
+        } else {
+            System.out.println("Chulu bhar paani mai dubi mar");
+        }
     }
 }
