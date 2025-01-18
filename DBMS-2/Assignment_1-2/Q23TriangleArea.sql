45dd8ecd48d0eaabe009f23a0229cd88f1d5fa6c
Vinay Thakor <vinay.th.24@gmail.com>
2025-01-18 13:54:05 +0530

    Replace Q19-Temp.sql with Q22-Temp.sql for temperature conversion; add Q23TriangleArea.sql to calculate the area of a triangle

---
+++ DBMS-2/Assignment_1-2/Q23TriangleArea.sql
@@ -0,0 +1,16 @@
+-- 23. Write a program to calculate the area of a triangle by accepting the 3 sides
+
+-- HERE s IS SEMIPERIMETER
+-- Area = √[s(s-a)(s-b)(s-c)]
+
+DECLARE
+    a NUMBER := &a;
+    b NUMBER := &b;
+    c NUMBER := &c;
+    s NUMBER := (a + b + c) / 2;
+    area FLOAT;
+BEGIN
+    area := SQRT(s * (s - a) * (s - b) * (s - c));
+    DBMS_OUTPUT.PUT_LINE('The area is: ' || area);
+END;
+/
