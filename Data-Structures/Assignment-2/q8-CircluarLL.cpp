7e89f0ce3d57f8cf9a09cd9b7672a56296e19986
Vinay Thakor <vinay.th.24@gmail.com>
2025-01-19 16:32:24 +0530

    Add q8-CircularLL.cpp to implement a circular single linked list with options to create, insert at the beginning, end, middle, and display the list

---
+++ Data-Structures/Assignment-2/q8-CircluarLL.cpp
@@ -0,0 +1,138 @@
+// Write a CPP program using switch case as below:
+// 1. Create a circular single linked list
+// 2. Insert a node in the beginning
+// 3. Insert a node in the end
+// 4. Insert a node in the middle
+// 5. Display circular linked list
+
+#include <iostream>
+using namespace std;
+class Node {
+public:
+  int data;
+  Node *next;
+};
+
+Node *start = NULL;
+Node *rear = NULL;
+
+void createCircularLL() {
+  if (start == NULL) {
+    Node *newNode = new Node;
+    cout << "Enter the data at start: ";
+    cin >> newNode->data;
+
+    newNode->next = newNode;
+    start = newNode;
+    rear = newNode;
+  }
+
+  int data;
+  while (true) {
+    cout << "Enter further data(to stop enter -1): ";
+    cin >> data;
+
+    if (data == -1)
+      break;
+
+    Node *newNode = new Node;
+
+    newNode->data = data;
+    newNode->next = start;
+
+    rear->next = newNode;
+    rear = newNode;
+  }
+}
+
+void insertAtStart(int data) {
+  Node *newNode = new Node;
+
+  newNode->data = data;
+  newNode->next = start;
+
+  start = newNode;
+  rear->next = start;
+}
+
+void insertAtEnd(int data) {
+  Node *newNode = new Node;
+
+  newNode->data = data;
+  newNode->next = start;
+
+  rear->next = newNode;
+  rear = newNode;
+}
+
+void insertInBetween(int data, int pos) {
+  Node *ptr = start;
+  Node *prePtr;
+  Node *newNode = new Node;
+  for (int i = 1; i < pos; i++) {
+    prePtr = ptr;
+    ptr = ptr->next;
+  }
+
+  newNode->data = data;
+  newNode->next = ptr;
+
+  prePtr->next = newNode;
+}
+
+void display() {
+  Node *ptr = start;
+  do {
+    cout << ptr->data << endl;
+    ptr = ptr->next;
+  } while (ptr != start);
+}
+
+int main() {
+  int choice;
+
+  while (true) {
+    cout << "\n1. Create a circular single linked list" << endl;
+    cout << "2. Insert a node in the beginning" << endl;
+    cout << "3. Insert a node in the end" << endl;
+    cout << "4. Insert a node in the middle" << endl;
+    cout << "5. Display circular linked list" << endl;
+    cout << "6. Exit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    cout << endl;
+
+    switch (choice) {
+    case 1:
+      createCircularLL();
+      break;
+    case 2:
+      int val;
+      cout << "Enter the value for new node at start: ";
+      cin >> val;
+      insertAtStart(val);
+      break;
+    case 3:
+      cout << "Enter the value for new node at end: ";
+      cin >> val;
+      insertAtEnd(val);
+      break;
+    case 4:
+      int pos;
+      cout << "Enter the value of position: ";
+      cin >> pos;
+      cout << "Enter the value for new node at " << pos << ": ";
+      cin >> val;
+      insertInBetween(val, pos);
+      break;
+    case 5:
+      display();
+      break;
+    case 6:
+      cout << "Exiting program...\n";
+      return 0;
+    default:
+      break;
+    }
+  }
+}
